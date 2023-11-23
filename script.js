668650a32988dcbe5df5e8a2bb9f81377a028356
MrSalis <philip.cost@hotmail.com>
2023-11-23 02:13:15 -0300

    from-real-to-all-currency-ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const converter = document.querySelector(".convert")
 const mySelect = document.querySelector(".currency-select")
-const mySelectL1 = document.querySelector(".currency-select-l1")
+
 
 function convertValues() {
 
@@ -8,6 +8,7 @@ function convertValues() {
     const toConvert = document.querySelector(".p-01")
     const toConverted = document.querySelector(".p-02")
 
+    
     const dolarToDay = 4.95
     const euroToDay = 5.27
     const libraToDay = 6.3
@@ -48,29 +49,6 @@ function convertValues() {
 
 }
 
-function convertValues1() {
-
-    const myInput = document.querySelector(".digite-o-valor").value
-    const toConvert = document.querySelector(".p-01")
-    const toConverted = document.querySelector(".p-02")
-
-    const realToDay = 1
-   
-    
-    if (mySelectL1.value == "real") {
-        toConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL"
-        }).format(myInput * realToDay)
-    }
-
-    toConvert.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(myInput)
- 
-
-}
 
 function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
@@ -104,42 +82,8 @@ function changeCurrency() {
     convertValues()
 }
 
-function changeCurrencyP1() {
-    const currencyNameP1 = document.getElementById("currency-name-p1")
-    const currencyImgP1 = document.querySelector(".brazil")
-
-    if (mySelectL1.value == "dolar") {
-        currencyNameP1.innerHTML = "$ Dólar americano"
-        currencyImgP1.src = "./assets/img-dolar-usa.png"
-    }
-
-    if (mySelectL1.value == "euro") {
-        currencyNameP1.innerHTML = "€ Euro"
-        currencyImgP1.src = "./assets/img-euro.png"
-    }
-
-    if (mySelectL1.value == "bitcoin") {
-        currencyNameP1.innerHTML = "₿ Bitcoin"
-        currencyImgP1.src = "./assets/bitcoin 1.png"
-    }
-
-    if (mySelectL1.value == "libra") {
-        currencyNameP1.innerHTML = "£ Libra"
-        currencyImgP1.src = "./assets/libra 1.png"
-    }
-
-    if (mySelectL1.value == "real") {
-        currencyNameP1.innerHTML = "R$ Real"
-        currencyImgP1.src = "./assets/img-real-brasil.png"
-    }
-
-    convertValues()
-}
-
-
-
 mySelect.addEventListener("change", changeCurrency)
 converter.addEventListener("click", convertValues)
-mySelectL1.addEventListener("change", changeCurrencyP1)
+
 
 
